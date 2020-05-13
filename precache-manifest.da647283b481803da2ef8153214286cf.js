71f1357a4cdbb99368262576f73914a9a00c28b3
Deepak raj <deepakraj5497@gmail.com>
2020-05-13 20:04:13 +0530

    Updates

---
+++ precache-manifest.da647283b481803da2ef8153214286cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ffd581a4e86938bf67d1838f835897",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "95dbdf0b7f27d5fdf836",
+    "url": "/react/static/css/main.14d34f23.chunk.css"
+  },
+  {
+    "revision": "8659f6eec1a8644a9207",
+    "url": "/react/static/js/2.e53b4b94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.e53b4b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95dbdf0b7f27d5fdf836",
+    "url": "/react/static/js/main.2e69b71a.chunk.js"
+  },
+  {
+    "revision": "2e35e2b07d73b1c41cf3",
+    "url": "/react/static/js/runtime-main.2ee29ac9.js"
+  },
+  {
+    "revision": "5276712a2e05ceb316c2c8c55095dba5",
+    "url": "/react/static/media/pic.5276712a.jpg"
+  }
+]);
