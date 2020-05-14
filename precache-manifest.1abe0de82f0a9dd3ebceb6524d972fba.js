eb5fe7927575746fd212b1b25f52114109dcfad1
Deepak raj <deepakraj5497@gmail.com>
2020-05-14 18:56:54 +0530

    Updates

--- precache-manifest.1abe0de82f0a9dd3ebceb6524d972fba.js
+++ precache-manifest.1abe0de82f0a9dd3ebceb6524d972fba.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2097a4133cb1fac359e5a915b3b97b81",
+    "revision": "9ca2560d47931a3a8167cffb9e050192",
     "url": "/react/index.html"
   },
   {
-    "revision": "bc5b9ee12c06764cbc6c",
+    "revision": "8cb546d98349eb9859c0",
     "url": "/react/static/css/main.14d34f23.chunk.css"
   },
   {
-    "revision": "8659f6eec1a8644a9207",
-    "url": "/react/static/js/2.e53b4b94.chunk.js"
+    "revision": "0fc6cf663d1a39ab9716",
+    "url": "/react/static/js/2.a1482e62.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react/static/js/2.e53b4b94.chunk.js.LICENSE.txt"
+    "url": "/react/static/js/2.a1482e62.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bc5b9ee12c06764cbc6c",
-    "url": "/react/static/js/main.aaf6bc85.chunk.js"
+    "revision": "8cb546d98349eb9859c0",
+    "url": "/react/static/js/main.1e9f32cd.chunk.js"
   },
   {
     "revision": "2e35e2b07d73b1c41cf3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.2bd2b4c509e51895e189294b9508e549.js"
+  "/react/precache-manifest.1abe0de82f0a9dd3ebceb6524d972fba.js"
 );
 
 self.addEventListener('message', (event) => {
